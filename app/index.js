928f5a45b2cf005ce692aaf9bdf07e71a0f62c96
Ernst Plüss <ernst.pluess@ernst-pluess.ch>
2016-06-11 18:50:49 +0200

    Made port configurable. Default is 80.

--- app/index.js
+++ app/index.js
@@ -5,6 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(80, function () {
+var port = process.env.PORT ? process.env.PORT : 80;
+console.log(port);
+
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
