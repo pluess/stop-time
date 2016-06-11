11fed55d282f5b91f59ea3e9a547f514136c9751
Ernst Plüss <ernst.pluess@ernst-pluess.ch>
2016-06-11 19:22:07 +0200

    Improoved ip and port detection for open shift.

--- app/index.js
+++ app/index.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var app = express();
 
+//Setup ip adress and port
+var ipaddress ;
+
+function initIPAdress() {
+    var adr = process.env.OPENSHIFT_NODEJS_IP;
+    if (typeof adr === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using localhost');
+            adr = 'localhost';
+    }
+
+    ipaddress = adr;
+}
+
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var port = process.env.PORT ? process.env.PORT : 80;
-console.log(port);
+initIPAdress(); //Setup IP adress before app.listen()
 
-app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, ipaddress, function() {
+        console.log('%s: Node server started on %s:%d ...', Date(Date.now() ), ipaddress, port);
 });
